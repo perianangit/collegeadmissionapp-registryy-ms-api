5bf95491dc3526d132b7c33a18471bcb3f0b9f2e
perianangit <periananpdkt@gmail.com>
2022-05-05 17:49:50 +0530

    second commit

---
+++ src/test/java/com/collegeadmissionapp/CollegeadmissionappRegistryyMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeadmissionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeadmissionappRegistryyMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
